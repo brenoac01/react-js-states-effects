5dfbb8ffc076f536415e4cbb2535b30808650ac6
brenoac01 <breno.ac01@yahoo.com>
2022-02-12 20:28:57 -0300

    aula react hooks

---
+++ aula-hooks/src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+//import SmartCounter from '../Smartcounter/Smartcounter';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter.js';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Ifoodcounter/>
+    </>
+  );
+}
+
+export default App;
